12a2a88e1018a7f4e7431b2ad31ca107e1d6ca5a
BAndrewGit <andrei.balan.2k1@gmail.com>
2025-10-24 14:26:18 +0300

    Refactor

--- src/main/java/Event/ex_interface/Event.java
+++
@@ -1,6 +0,0 @@
-package Event.ex_interface;
-
-public interface Event {
-    Long getTimeStamp();
-    void process();
-}
--- src/main/java/org/example/TreeType.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public enum TreeType {
-    OAK,
-    MAPLE,
-    PINE,
-    WALNUT,
-    PECAN,
-}
